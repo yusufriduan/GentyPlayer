a3a05ad2d20e34ac6c77aa5574a8b2018983bf49
yusufriduan <yusufriduan@outlook.com>
2026-04-14 13:21:27 +0800

    FIX: add methods for api calls for playbacks

--- app/api/spotify/pause/route.ts
+++ app/api/spotify/pause/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: NextRequest) {
 
     try {
         const response = await fetch('https://api.spotify.com/v1/me/player/pause', {
-            method: 'PUT',
+            method: 'POST',
             headers: {
                 Authorization: authHeader,
             },
--- app/api/spotify/play/route.ts
+++ app/api/spotify/play/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: NextRequest) {
 
     try {
         const response = await fetch('https://api.spotify.com/v1/me/player/play', {
-            method: 'PUT',
+            method: 'POST',
             headers: {
                 Authorization: authHeader,
             },
